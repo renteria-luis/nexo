ac5cf97d02d0b94cd552a3778683c7940a1304c5
Luis Renteria <luisrenterialezano@gmail.com>
2026-09-13 12:35:41 -0400

    feat(ui): add empty home screen

--- App.tsx
+++ App.tsx
@@ -3,18 +3,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Text style={styles.title}>nexo</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 20,
   },
 });
